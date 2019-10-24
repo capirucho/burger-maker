7ed93f355ee324ad7d730a1345248b9f8c96a3b6
rfigueroa <friedbeings@gmail.com>
2019-10-24 01:28:40 -0500

    added logo, navigation components

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
-import classes from './Layout.module.css'
+import classes from './Layout.module.css';
+import Toolbar from '../navigation/toolbar/Toolbar';
+
 
 const layout = ( props ) => (
     <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toolbar />
         <main className={classes.content}>
             {props.children}
         </main>       
